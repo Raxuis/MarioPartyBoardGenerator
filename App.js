b33132afd66982a397ff58e8576f2ae4398b594c
Raxuis <raclot.raphael@gmail.com>
2025-02-03 16:02:09 +0100

    fix(animated image): fix issue with buttons moving when animating

--- App.js
+++ App.js
@@ -12,10 +12,18 @@ import Animated, {
     withRepeat,
     withSpring
 } from 'react-native-reanimated';
+import * as Haptics from "expo-haptics";
+import {data} from "./constants";
 
 
 export default function App() {
-    const {map, generateMap, setMap} = useStore();
+    const {
+        map,
+        generateMap,
+        setMap,
+        page,
+        setPage
+    } = useStore();
     const [loaded, error] = useFonts({
         'Super-Mario': require('./assets/fonts/SuperMario256.ttf'),
         'ShinGoPro': require('./assets/fonts/AOTFShinGoProRegular.otf'),
@@ -30,8 +38,8 @@ export default function App() {
     const opacity = useSharedValue(0);
     const width = useSharedValue(0);
     const height = useSharedValue(0);
-    const iconWidth = useSharedValue(175);
-    const iconHeight = useSharedValue(175);
+    const iconWidth = useSharedValue(180);
+    const iconHeight = useSharedValue(180);
 
     useEffect(() => {
         iconHeight.value = withRepeat(withTiming(200, {duration: 1000}), -1, true);
@@ -44,6 +52,10 @@ export default function App() {
         }
     }, [loaded, error]);
 
+    useEffect(() => {
+        console.log(page)
+    }, [page]);
+
     if (!loaded && !error) {
         return null;
     }
@@ -51,129 +63,159 @@ export default function App() {
     return (
         <View style={styles.container}>
             {
-                map.name === "" ? (
-                    <View style={styles.randomMapContainer}>
-                        <Animated.Image
-                            source={require('./assets/icon.png')}
-                            style={{
-                                width: iconWidth,
-                                height: iconHeight,
-                                resizeMode: 'contain',
-                            }}
-                        />
-                        <CustomButton
-                            style={styles.CTAButton}
-                            textStyle={styles.CTAButtonText}
-                            onPress={() => {
-                                generateMap();
-                                opacity.value = withTiming(1, {
-                                    duration: 300,
-                                    easing: Easing.elastic(1),
-                                    reduceMotion: ReduceMotion.System,
-                                });
-                                width.value = withTiming(200, {
-                                    duration: 300,
-                                    easing: Easing.elastic(1),
-                                    reduceMotion: ReduceMotion.System,
-                                });
-                                height.value = withTiming(200, {
-                                    duration: 300,
-                                    easing: Easing.elastic(1),
-                                    reduceMotion: ReduceMotion.System,
-                                });
-                            }}
-                        >
-                            Choisir une carte
-                        </CustomButton>
-                    </View>
-                ) : (
-                    <View style={styles.container}>
-                        <Image
-                            source={map.boardView}
-                            style={[styles.fullSize, {
-                                position: "absolute",
-                                top: 0,
-                                left: 0,
-                                right: 0,
-                                bottom: 0,
-                                resizeMode: "cover",
-                            }]}
-                        />
-                        <View
-                            style={[styles.fullSize, {
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                backgroundColor: "rgba(0, 0, 0, 0.8)",
-                            }]}>
-                            <View
-                                style={[styles.randomMapContainer, {
-                                    padding: 20,
-                                }]}>
+                page === "home" ?
+                    map.name === "" ? (
+                        <View style={[styles.randomMapContainer]}>
+                            <View style={styles.imageContainer}>
                                 <Animated.Image
-                                    source={map.boardIcon}
+                                    source={require('./assets/icon.png')}
                                     style={{
-                                        width: width,
-                                        height: height,
-                                        opacity: opacity,
+                                        width: iconWidth,
+                                        height: iconHeight,
                                         resizeMode: 'contain',
                                     }}
                                 />
-                                <Text style={{
-                                    fontFamily: "ShinGoPro-Bold",
-                                    fontSize: 24,
-                                    textAlign: "center",
-                                    color: "white",
-                                    marginTop: 20,
-                                }}>
-                                    {map.name}
-                                </Text>
-                                <Text style={{
-                                    fontFamily: "ShinGoPro",
-                                    fontSize: 12,
-                                    textAlign: "center",
-                                    paddingTop: 10,
-                                    opacity: 0.7,
-                                    color: "white"
-                                }}>
-                                    {map.description}
-                                </Text>
-                                <CustomButton
-                                    style={styles.CTAButton}
-                                    textStyle={styles.CTAButtonText}
-                                    onPress={() => {
-                                        setMap({
-                                            id: 0,
-                                            name: "",
-                                            description: "",
-                                            boardView: "",
-                                            boardIcon: ""
-                                        })
-                                        opacity.value = withTiming(0, {
-                                            duration: 300,
-                                            easing: Easing.elastic(1),
-                                            reduceMotion: ReduceMotion.System,
-                                        });
-                                        width.value = withTiming(0, {
-                                            duration: 300,
-                                            easing: Easing.elastic(1),
-                                            reduceMotion: ReduceMotion.System,
-                                        });
-                                        height.value = withTiming(0, {
-                                            duration: 300,
-                                            easing: Easing.elastic(1),
-                                            reduceMotion: ReduceMotion.System,
-                                        });
-                                        iconHeight.value = 175;
-                                        iconWidth.value = 175;
-                                    }}
-                                >
-                                    Réinitialiser
-                                </CustomButton>
+                            </View>
+                            <CustomButton
+                                style={styles.CTAButton}
+                                textStyle={styles.CTAButtonText}
+                                onPress={() => {
+                                    generateMap();
+                                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+                                    opacity.value = withTiming(1, {
+                                        duration: 300,
+                                        easing: Easing.elastic(1),
+                                        reduceMotion: ReduceMotion.System,
+                                    });
+                                    width.value = withTiming(200, {
+                                        duration: 300,
+                                        easing: Easing.elastic(1),
+                                        reduceMotion: ReduceMotion.System,
+                                    });
+                                    height.value = withTiming(200, {
+                                        duration: 300,
+                                        easing: Easing.elastic(1),
+                                        reduceMotion: ReduceMotion.System,
+                                    });
+                                }}
+                            >
+                                Choisir une carte
+                            </CustomButton>
+                            <CustomButton
+                                style={[styles.CTAButton, {marginTop: 0}]}
+                                textStyle={styles.CTAButtonText}
+                                onPress={() => {
+                                    setPage("maps");
+                                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+                                }}
+                            >
+                                Voir toutes les cartes
+                            </CustomButton>
+                        </View>
+                    ) : (
+                        <View style={styles.container}>
+                            <Image
+                                source={map.boardView}
+                                style={[styles.fullSize, {
+                                    position: "absolute",
+                                    top: 0,
+                                    left: 0,
+                                    right: 0,
+                                    bottom: 0,
+                                    resizeMode: "cover",
+                                }]}
+                            />
+                            <View
+                                style={[styles.fullSize, {
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    backgroundColor: "rgba(0, 0, 0, 0.8)",
+                                }]}>
+                                <View
+                                    style={[styles.randomMapContainer, {
+                                        padding: 20,
+                                    }]}>
+                                    <Animated.Image
+                                        source={map.boardIcon}
+                                        style={{
+                                            width: width,
+                                            height: height,
+                                            opacity: opacity,
+                                            resizeMode: 'contain',
+                                        }}
+                                    />
+                                    <Text style={{
+                                        fontFamily: "ShinGoPro-Bold",
+                                        fontSize: 24,
+                                        textAlign: "center",
+                                        color: "white",
+                                        marginTop: 20,
+                                    }}>
+                                        {map.name}
+                                    </Text>
+                                    <Text style={{
+                                        fontFamily: "ShinGoPro",
+                                        fontSize: 12,
+                                        textAlign: "center",
+                                        paddingTop: 10,
+                                        opacity: 0.7,
+                                        color: "white"
+                                    }}>
+                                        {map.description}
+                                    </Text>
+                                    <CustomButton
+                                        style={styles.CTAButton}
+                                        textStyle={styles.CTAButtonText}
+                                        onPress={() => {
+                                            setMap({
+                                                id: 0,
+                                                name: "",
+                                                description: "",
+                                                boardView: "",
+                                                boardIcon: ""
+                                            })
+                                            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+                                            opacity.value = withTiming(0, {
+                                                duration: 300,
+                                                easing: Easing.elastic(1),
+                                                reduceMotion: ReduceMotion.System,
+                                            });
+                                            width.value = withTiming(0, {
+                                                duration: 300,
+                                                easing: Easing.elastic(1),
+                                                reduceMotion: ReduceMotion.System,
+                                            });
+                                            height.value = withTiming(0, {
+                                                duration: 300,
+                                                easing: Easing.elastic(1),
+                                                reduceMotion: ReduceMotion.System,
+                                            });
+                                            iconHeight.value = 180;
+                                            iconWidth.value = 180;
+                                        }}
+                                    >
+                                        Réinitialiser
+                                    </CustomButton>
+                                </View>
                             </View>
                         </View>
-                    </View>
-                )
+                    ) : page === "maps" ? (
+                        <View style={styles.container}>
+                            <Text>Maps</Text>
+                            {
+                                data.map((item, index) => (
+                                    <View key={index}>
+                                        <Text>{item.name}</Text>
+                                    </View>
+                                ))
+                            }
+                        </View>
+                    ) : (
+                        <View style={styles.container}>
+                            <Text>Page not found</Text>
+                        </View>
+                    )
             }
         </View>
     )
@@ -210,5 +252,12 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: "bold",
         fontFamily: "Super-Mario",
+    },
+    imageContainer: {
+        height: 200,
+        width: 200,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
     }
 });
--- store/store.js
+++ store/store.js
@@ -23,4 +23,6 @@ export const useStore = create((set) => ({
             }
         });
     },
+    page: "home",
+    setPage: (page) => set({page}),
 }));
